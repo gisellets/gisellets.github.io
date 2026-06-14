da1fc9eee41c780ec719002ff29cdf37634e74fa
Giselle <gisellevi002@gmail.com>
2026-06-14 18:19:16 -0400

    fix index

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
 const roles = [
-    "Computer Science Student",
-    "Leader",
-    "Problem Solver",
-    "Web Developer",
-    "Lifelong Learner"
+  "a Computer Science Student",
+  "a Lifelong Learner",
+  "a Problem Solver",
+  "a Leader",
+  "a Web Developer"
 ];
 
-let index = 0;
+let i = 0;
+let j = 0;
+let currentText = "";
+let isDeleting = false;
 
-const btn = document.getElementById("identityBtn");
-const text = document.getElementById("identityText");
+function type() {
+  const display = document.getElementById("type-text");
 
-btn.addEventListener("click", () => {
-    index = (index + 1) % roles.length;
-    text.textContent = roles[index];
-});
+  let fullText = roles[i];
 
+  if (isDeleting) {
+    currentText = fullText.substring(0, j--);
+  } else {
+    currentText = fullText.substring(0, j++);
+  }
+
+  display.textContent = currentText;
+
+  let speed = isDeleting ? 60 : 90;
+
+  if (!isDeleting && currentText === fullText) {
+    speed = 1200;
+    isDeleting = true;
+  } else if (isDeleting && currentText === "") {
+    isDeleting = false;
+    i = (i + 1) % roles.length;
+    speed = 400;
+  }
+
+  setTimeout(type, speed);
+}
+
+type();
 
